841d9b89e3c5eb65e55396f491759c1838093f03
Phan Huu Phuoc Loc <109939299+locphp@users.noreply.github.com>
2025-12-15 17:16:35 +0700

    test1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
 
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
 # 1. Tạo Security Group (Mở cửa cho web và k3s)
 resource "aws_security_group" "k3s_sg" {
@@ -60,7 +60,22 @@ resource "aws_instance" "k3s_server" {
   ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS (us-east-1)
   instance_type = "t3.medium"
   key_name      = "dolciluxe-key" # Tên Key Pair (Xem hướng dẫn tạo ở dưới)
-  
+
+  user_data = <<-EOF
+            #!/bin/bash
+            # Cài đặt K3s (Kubernetes nhẹ)
+            curl -sfL https://get.k3s.io | sh -
+            
+            # Cấu hình quyền truy cập config cho user ubuntu
+            mkdir -p /home/ubuntu/.kube
+            cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/.kube/config
+            chown ubuntu:ubuntu /home/ubuntu/.kube/config
+            chmod 600 /home/ubuntu/.kube/config
+            
+            # Cho phép ghi đè biến môi trường KUBECONFIG
+            echo 'export KUBECONFIG=/home/ubuntu/.kube/config' >> /home/ubuntu/.bashrc
+            EOF
+
   vpc_security_group_ids = [aws_security_group.k3s_sg.id]
 
   tags = {
